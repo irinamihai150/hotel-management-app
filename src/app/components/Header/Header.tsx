d486366994869f2796e46f885beb863e47622e53
irinamihai150 <irinamihai150@gmail.com>
2024-01-14 13:44:11 +0000

    initial commit

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Header = () => {
 	return (
-		<header className='py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between'></header>
+		<header className='py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify'></header>
 	)
 }
 
