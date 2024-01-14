e7479a85e4e14221d0cac0b2694376ad79e88c92
irinamihai150 <irinamihai150@gmail.com>
2024-01-14 13:42:35 +0000

    initial commit

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Header = () => {
+	return (
+		<header className='py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between'></header>
+	)
+}
+
+export default Header
